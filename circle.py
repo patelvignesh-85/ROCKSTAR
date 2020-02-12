abb9af6d963e8efe15493b8519dd5b9b60d2384f
patelvignesh-85 <60962775+patelvignesh-85@users.noreply.github.com>
2020-02-12 15:21:45 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,13 @@
+class Circle:
+    def CalculateArea(self):
+        print("Enter radius")
+        self.s=float(input())
+        area=3.14*self.s*self.s
+        print("Area of Circle is=%f"%(area))
+    def CalculatePerimeter(self):
+        perimeter=2*3.14*self.s
+        print("perimeter of Circle is=%f"%(perimeter))
+c=Circle()
+c.CalculateArea()
+c.CalculatePerimeter()
+        
