ab7fac1694694898f054ee2856c47479b9317dda
patelvignesh-85 <60962775+patelvignesh-85@users.noreply.github.com>
2020-03-14 13:32:33 +0530

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
 class Circle:
-    def CalculateArea(self):
+  def CalculateArea(self):
         print("Enter radius")
         self.s=float(input())
         area=3.14*self.s*self.s
         print("Area of Circle is=%f"%(area))
-    def CalculatePerimeter(self):
+  def CalculatePerimeter(self):
         perimeter=2*3.14*self.s
         print("perimeter of Circle is=%f"%(perimeter))
 c=Circle()
